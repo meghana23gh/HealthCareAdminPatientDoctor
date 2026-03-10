12e24d119d7b4183a8df750058169573c7a61cfd
Yogesh Patil <yp901981@gmail.com>
2026-03-10 13:15:46 +0530

    apiresponse added

--- src/main/java/nimblix/in/HealthCareHub/response/ApiResponse.java
+++ src/main/java/nimblix/in/HealthCareHub/response/ApiResponse.java
@@ -1,5 +1,13 @@
 package nimblix.in.HealthCareHub.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
 
     private String status;
